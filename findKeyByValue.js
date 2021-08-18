4f51dc7b0b819d96593f5fec8b6f4396a902301f
Rupi-Jain <rupi.jain@gmail.com>
2021-08-18 18:07:28 -0400

    first commit to findKeyByValue.js file

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) 
+{
+  if(actual === expected)
+  {
+    console.log(`💚💚💚 Assertion Passed: ${actual}  ===  ${expected}`);
+  }
+  else
+  {
+    console.log(`❤️‍🩹❤️‍🩹❤️‍🩹 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  for(let key in obj) {
+    if(obj[key] === val)
+      return key;
+  }
+  return ;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
